eae28fb61109a866e7e85f075967441dbf152c13
theburningmonk <theburningmonk@gmail.com>
2020-11-09 15:55:52 +0100

    feat: add tests for getLikes query

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -238,16 +238,7 @@ const a_user_calls_getImageUploadUrl = async (user, extension, contentType) => {
 const a_user_calls_tweet = async (user, text) => {
   const tweet = `mutation tweet($text: String!) {
     tweet(text: $text) {
-      id
-      profile {
-        ... iProfileFields
-      }
-      createdAt
-      text
-      replies
-      likes
-      retweets
-      liked
+      ... tweetFields
     }
   }`
   const variables = {
@@ -339,6 +330,29 @@ const a_user_calls_unlike = async (user, tweetId) => {
   return result
 }
 
+const a_user_calls_getLikes = async (user, userId, limit, nextToken) => {
+  const getLikes = `query getLikes($userId: ID!, $limit: Int!, $nextToken: String) {
+    getLikes(userId: $userId, limit: $limit, nextToken: $nextToken) {
+      nextToken
+      tweets {
+        ... iTweetFields
+      }
+    }
+  }`
+  const variables = {
+    userId,
+    limit,
+    nextToken
+  }
+
+  const data = await GraphQL(process.env.API_URL, getLikes, variables, user.accessToken)
+  const result = data.getLikes
+
+  console.log(`[${user.username}] - fetched likes`)
+
+  return result
+}
+
 module.exports = {
   we_invoke_confirmUserSignup,
   we_invoke_getImageUploadUrl,
@@ -352,5 +366,6 @@ module.exports = {
   a_user_calls_getTweets,
   a_user_calls_getMyTimeline,
   a_user_calls_like,
-  a_user_calls_unlike
+  a_user_calls_unlike,
+  a_user_calls_getLikes
 }
--- __tests__/test_cases/e2e/tweeting.js
+++ __tests__/test_cases/e2e/tweeting.js
@@ -94,6 +94,22 @@ describe('Given an authenticated user', () => {
           })
       })
 
+      it('Should see this tweet when he calls getLikes', async () => {
+        const { tweets, nextToken } = await when.a_user_calls_getLikes(user, user.username, 25)
+
+        expect(nextToken).toBeNull()
+        expect(tweets).toHaveLength(1)
+        expect(tweets[0]).toMatchObject({
+          ...tweet,
+          liked: true,
+          likes: 1,
+          profile: {
+            ...tweet.profile,
+            likesCounts: 1
+          }
+        })
+      })
+
       describe('When he unlikes the tweet', () => {
         beforeAll(async () => {
           await when.a_user_calls_unlike(user, tweet.id)
@@ -113,6 +129,13 @@ describe('Given an authenticated user', () => {
             .toMatchObject({
               message: expect.stringContaining('DynamoDB transaction error')
             })
+        })
+
+        it('Should not see this tweet when he calls getLikes anymore', async () => {
+          const { tweets, nextToken } = await when.a_user_calls_getLikes(user, user.username, 25)
+
+          expect(nextToken).toBeNull()
+          expect(tweets).toHaveLength(0)
         })
       })
     })
