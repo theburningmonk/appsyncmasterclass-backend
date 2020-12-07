02d240aeb065d1c45383ee27443a19eaa76e08b9
theburningmonk <theburningmonk@gmail.com>
2020-12-07 16:24:06 +0100

    feat: added unit test for hydrateFollowers.request

--- __tests__/steps/given.js
+++ __tests__/steps/given.js
@@ -21,7 +21,7 @@ const a_random_user = () => {
   }
 }
 
-const an_appsync_context = (identity, args, result, source, info) => {
+const an_appsync_context = (identity, args, result, source, info, prev) => {
   const util = velocityUtil.create([], new Date(), Object())
   const context = {
     identity,
@@ -29,7 +29,8 @@ const an_appsync_context = (identity, args, result, source, info) => {
     arguments: args,
     result,
     source,
-    info
+    info,
+    prev
   }
   return {
     context,
---
+++ __tests__/test_cases/unit/hydrateFollowers.request.js
@@ -0,0 +1,56 @@
+const given = require('../../steps/given')
+const when = require('../../steps/when')
+const chance = require('chance').Chance()
+const path = require('path')
+
+describe('hydrateFollowers.request template', () => {
+  it("Should return empty array if prev.result.relationships is empty", () => {
+    const templatePath = path.resolve(__dirname, '../../../mapping-templates/hydrateFollowers.request.vtl')
+
+    const username = chance.guid()
+    const prev = {
+      result: {
+        relationships: []
+      }
+    }
+    const context = given.an_appsync_context({ username }, {}, {}, {}, {}, prev)
+    const result = when.we_invoke_an_appsync_template(templatePath, context)
+
+    expect(result).toEqual([])
+  })
+
+  it("Should convert relationships to BatchGetItem keys", () => {
+    const templatePath = path.resolve(__dirname, '../../../mapping-templates/hydrateFollowers.request.vtl')
+
+    const username = chance.guid()
+    const userId = chance.guid()
+    const otherUserId = chance.guid()
+    const relationships = [{
+      userId,
+      sk: `FOLLOWS_${otherUserId}`,
+      otherUserId
+    }]
+    const prev = {
+      result: {
+        relationships
+      }
+    }
+    const context = given.an_appsync_context({ username }, {}, {}, {}, {}, prev)
+    const result = when.we_invoke_an_appsync_template(templatePath, context)
+
+    expect(result).toEqual({
+      "version" : "2018-05-29",
+      "operation" : "BatchGetItem",
+      "tables" : {
+        "${UsersTable}": {
+          "keys": [{
+            "id": {
+              "S": userId
+            }
+          }],
+          "consistentRead": false
+        }
+      }
+    })
+  })
+})
