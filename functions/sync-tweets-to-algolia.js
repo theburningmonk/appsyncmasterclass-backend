5f8800c6759cfc80d111be608be8214ebb30cd41
theburningmonk <theburningmonk@gmail.com>
2021-02-18 14:23:45 +0100

    feat: securely handle algolia admin key

--- functions/sync-tweets-to-algolia.js
+++ functions/sync-tweets-to-algolia.js
@@ -1,10 +1,14 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb')
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
 const { initTweetsIndex } = require('../lib/algolia')
 const { TweetTypes } = require('../lib/constants')
 
-const { STAGE, ALGOLIA_APP_ID, ALGOLIA_WRITE_KEY } = process.env
+const { STAGE } = process.env
 
-module.exports.handler = async (event) => {
-  const index = await initTweetsIndex(ALGOLIA_APP_ID, ALGOLIA_WRITE_KEY, STAGE)
+module.exports.handler = middy(async (event, context) => {
+  const index = await initTweetsIndex(
+    context.ALGOLIA_APP_ID, context.ALGOLIA_WRITE_KEY, STAGE)
 
   for (const record of event.Records) {
     if (record.eventName === 'INSERT' || record.eventName === 'MODIFY') {
@@ -27,4 +31,13 @@ module.exports.handler = async (event) => {
       await index.deleteObjects([tweet.id])
     }
   }
-}
+}).use(ssm({
+  cache: true,
+  cacheExpiryInMillis: 5 * 60 * 1000, // 5 mins
+  names: {
+    ALGOLIA_APP_ID: `/${STAGE}/algolia-app-id`,
+    ALGOLIA_WRITE_KEY: `/${STAGE}/algolia-admin-key`
+  },
+  setToContext: true,
+  throwOnFailedCall: true
+}))
--- functions/sync-users-to-algolia.js
+++ functions/sync-users-to-algolia.js
@@ -1,9 +1,13 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb')
 const { initUsersIndex } = require('../lib/algolia')
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
 
-const { STAGE, ALGOLIA_APP_ID, ALGOLIA_WRITE_KEY } = process.env
+const { STAGE } = process.env
 
-module.exports.handler = async (event) => {
-  const index = await initUsersIndex(ALGOLIA_APP_ID, ALGOLIA_WRITE_KEY, STAGE)
+module.exports.handler = middy(async (event, context) => {
+  const index = await initUsersIndex(
+    context.ALGOLIA_APP_ID, context.ALGOLIA_WRITE_KEY, STAGE)
 
   for (const record of event.Records) {
     if (record.eventName === 'INSERT' || record.eventName === 'MODIFY') {
@@ -18,4 +22,13 @@ module.exports.handler = async (event) => {
       await index.deleteObjects([profile.id])
     }
   }
-}
+}).use(ssm({
+  cache: true,
+  cacheExpiryInMillis: 5 * 60 * 1000, // 5 mins
+  names: {
+    ALGOLIA_APP_ID: `/${STAGE}/algolia-app-id`,
+    ALGOLIA_WRITE_KEY: `/${STAGE}/algolia-admin-key`
+  },
+  setToContext: true,
+  throwOnFailedCall: true
+}))
