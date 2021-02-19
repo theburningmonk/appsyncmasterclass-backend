7dc94b4c5fa4ee323d31e21ec5c893da973e1fa5
theburningmonk <theburningmonk@gmail.com>
2021-02-19 01:20:28 +0100

    feat: implement the search query

---
+++ functions/search.js
@@ -0,0 +1,104 @@
+const chance = require('chance').Chance()
+const { initUsersIndex, initTweetsIndex } = require('../lib/algolia')
+const { SearchModes } = require('../lib/constants')
+const middy = require('@middy/core')
+const ssm = require('@middy/ssm')
+
+const { STAGE } = process.env
+
+module.exports.handler = middy(async (event, context) => {
+  const userId = event.identity.username
+  const { query, mode, limit, nextToken } = event.arguments
+
+  switch (mode) {
+    case SearchModes.PEOPLE:
+      return await searchPeople(context, userId, query, limit, nextToken)
+    case SearchModes.LATEST:
+      return await searchLatest(context, query, limit, nextToken)
+    default:
+      throw new Error('Only "People" and "Latest" search modes are supported right now')
+  }
+}).use(ssm({
+  cache: true,
+  cacheExpiryInMillis: 5 * 60 * 1000, // 5 mins
+  names: {
+    ALGOLIA_APP_ID: `/${STAGE}/algolia-app-id`,
+    ALGOLIA_WRITE_KEY: `/${STAGE}/algolia-admin-key`
+  },
+  setToContext: true,
+  throwOnFailedCall: true
+}))
+
+async function searchPeople(context, userId, query, limit, nextToken) {
+  const index = await initUsersIndex(
+    context.ALGOLIA_APP_ID, context.ALGOLIA_WRITE_KEY, STAGE)
+
+  const searchParams = parseNextToken(nextToken) || {
+    hitsPerPage: limit,
+    page: 0
+  }
+
+  const { hits, page, nbPages } = await index.search(query, searchParams)
+  hits.forEach(x => {
+    x.__typename = x.id === userId ? 'MyProfile' : 'OtherProfile'
+  })
+
+  let nextSearchParams
+  if (page + 1 >= nbPages) {
+    nextSearchParams = null
+  } else {
+    nextSearchParams = Object.assign({}, searchParams, { page: page + 1 })
+  }
+
+  return {
+    results: hits,
+    nextToken: genNextToken(nextSearchParams)
+  }
+}
+
+async function searchLatest(context, query, limit, nextToken) {
+  const index = await initTweetsIndex(
+    context.ALGOLIA_APP_ID, context.ALGOLIA_WRITE_KEY, STAGE)
+
+  const searchParams = parseNextToken(nextToken) || {
+    hitsPerPage: limit,
+    page: 0
+  }
+
+  const { hits, page, nbPages } = await index.search(query, searchParams)
+
+  let nextSearchParams
+  if (page + 1 >= nbPages) {
+    nextSearchParams = null
+  } else {
+    nextSearchParams = Object.assign({}, searchParams, { page: page + 1 })
+  }
+
+  return {
+    results: hits,
+    nextToken: genNextToken(nextSearchParams)
+  }
+}
+
+function parseNextToken(nextToken) {
+  if (!nextToken) {
+    return undefined
+  }
+
+  const token = Buffer.from(nextToken, 'base64').toString()
+  const searchParams = JSON.parse(token)
+  delete searchParams.random
+
+  return searchParams
+}
+
+function genNextToken(searchParams) {
+  if (!searchParams) {
+    return null
+  }
+
+  const payload = Object.assign(
+    {}, searchParams, { random: chance.string({ length: 16 }) })
+  const token = JSON.stringify(payload)
+  return Buffer.from(token).toString('base64')
+}
---
+++ scripts/dummy-update-users.js
@@ -0,0 +1,40 @@
+const AWS = require('aws-sdk')
+AWS.config.region = 'eu-west-1'
+const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+const { resolve } = require('path')
+require('dotenv').config({
+  path: resolve(__dirname, '../.env'),
+})
+
+const run = async () => {
+  const loop = async (exclusiveStartKey) => {
+    const resp = await DynamoDB.scan({
+      TableName: process.env.USERS_TABLE,
+      ExclusiveStartKey: exclusiveStartKey,
+      Limit: 100
+    }).promise()
+
+    const promises = resp.Items.map(async x => {
+      await DynamoDB.update({
+        TableName: process.env.USERS_TABLE,
+        Key: {
+          id: x.id
+        },
+        UpdateExpression: "SET lastUpdated = :now",
+        ExpressionAttributeValues: {
+          ":now": new Date().toJSON()
+        }
+      }).promise()
+    })
+    await Promise.all(promises)
+
+    if (resp.LastEvaluatedKey) {
+      return await loop(resp.LastEvaluatedKey)
+    }
+  }
+
+  await loop()
+}
+
+run().then(x => console.log('all done'))
