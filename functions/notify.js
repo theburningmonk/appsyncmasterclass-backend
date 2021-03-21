0ba399e11f0981233a47fc5e9d8195c0b7d775ad
theburningmonk <theburningmonk@gmail.com>
2021-03-21 22:09:11 +0100

    feat: add subscription for mentions

--- functions/notify.js
+++ functions/notify.js
@@ -1,9 +1,11 @@
+const _ = require('lodash')
 const DynamoDB = require('aws-sdk/clients/dynamodb')
 const { TweetTypes } = require('../lib/constants')
 const graphql = require('graphql-tag')
 const { mutate } = require('../lib/graphql')
 const ulid = require('ulid')
-const { getTweetById } = require('../lib/tweets')
+const { getTweetById, extractMentions } = require('../lib/tweets')
+const { getUserByScreenName } = require('../lib/users')
 
 module.exports.handler = async (event) => {
   for (const record of event.Records) {
@@ -14,6 +16,13 @@ module.exports.handler = async (event) => {
         case TweetTypes.RETWEET:
           await notifyRetweet(tweet)
           break
+      }
+
+      if (tweet.text) {
+        const mentions = extractMentions(tweet.text)
+        if (!_.isEmpty(mentions)) {
+          await notifyMentioned(mentions, tweet)
+        }
       }
     }
   }
@@ -53,4 +62,44 @@ async function notifyRetweet(tweet) {
     retweetId: tweet.id,
     retweetedBy: tweet.creator
   })
+}
+
+async function notifyMentioned(screenNames, tweet) {
+  const promises = screenNames.map(async (screenName) => {
+    const user = await getUserByScreenName(screenName.replace('@', ''))
+    if (!user) {
+      return
+    }
+
+    await mutate(graphql `mutation notifyMentioned(
+      $id: ID!
+      $userId: ID!
+      $mentionedBy: ID!
+      $mentionedByTweetId: ID!
+    ) {
+      notifyMentioned(
+        id: $id
+        userId: $userId
+        mentionedBy: $mentionedBy
+        mentionedByTweetId: $mentionedByTweetId
+      ) {
+        __typename
+        ... on Mentioned {
+          id
+          type
+          userId
+          mentionedBy
+          mentionedByTweetId
+          createdAt
+        }
+      }
+    }`, {
+      id: ulid.ulid(),
+      userId: user.id,
+      mentionedBy: tweet.creator,
+      mentionedByTweetId: tweet.id
+    })
+  })
+  
+  await Promise.all(promises)
 }
