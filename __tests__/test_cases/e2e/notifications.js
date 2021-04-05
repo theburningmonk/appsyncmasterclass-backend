8600caf96d570fe7d1d5e543cf415d1b76412385
theburningmonk <theburningmonk@gmail.com>
2021-04-05 18:26:29 +0200

    test: add e2e tests for likes, replies and mentions

--- __tests__/test_cases/e2e/notifications.js
+++ __tests__/test_cases/e2e/notifications.js
@@ -6,14 +6,16 @@ const retry = require('async-retry')
 const chance = require('chance').Chance()
 const { AWSAppSyncClient, AUTH_TYPE } = require('aws-appsync')
 require('isomorphic-fetch')
+console.warn = jest.fn()
 console.error = jest.fn()
 
 describe('Given two authenticated users', () => {
-  let userA, userB, userAsTweet
+  let userA, userB, userAsProfile, userAsTweet
   const text = chance.string({ length: 16 })
 
   beforeAll(async () => {
     userA = await given.an_authenticated_user()
+    userAsProfile = await when.a_user_calls_getMyProfile(userA)
     userB = await given.an_authenticated_user()
     userAsTweet = await when.a_user_calls_tweet(userA, text)
   })
@@ -49,6 +51,22 @@ describe('Given two authenticated users', () => {
                 retweetedBy
                 retweetId
               }
+              
+              ... on Liked {
+                tweetId
+                likedBy
+              }
+
+              ... on Replied {
+                tweetId
+                replyTweetId
+                repliedBy
+              }
+
+              ... on Mentioned {
+                mentionedByTweetId
+                mentionedBy
+              }
             }
           }
         `,
@@ -64,6 +82,30 @@ describe('Given two authenticated users', () => {
 
     afterAll(() => {
       subscription.unsubscribe()
+    })
+
+    describe("When user B likes user A's tweet", () => {
+      beforeAll(async () => {
+        await when.a_user_calls_like(userB, userAsTweet.id)
+      })
+
+      it('User A should receive a notification', async () => {
+        await retry(async () => {
+          expect(notifications).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                type: 'Liked',
+                userId: userA.username,
+                tweetId: userAsTweet.id,
+                likedBy: userB.username
+              })
+            ])
+          )
+        }, {
+          retries: 10,
+          maxTimeout: 1000
+        })
+      }, 15000)
     })
 
     describe("When user B retweets user A's tweet", () => {
@@ -82,6 +124,60 @@ describe('Given two authenticated users', () => {
                 tweetId: userAsTweet.id,
                 retweetId: userBsRetweet.id,
                 retweetedBy: userB.username
+              })
+            ])
+          )
+        }, {
+          retries: 10,
+          maxTimeout: 1000
+        })
+      }, 15000)
+    })
+
+    describe("When user B replied to user A's tweet", () => {
+      let userBsReply
+      const replyText = chance.string({ length: 16 })
+      beforeAll(async () => {
+        userBsReply = await when.a_user_calls_reply(userB, userAsTweet.id, replyText)
+      })
+
+      it('User A should receive a notification', async () => {
+        await retry(async () => {
+          expect(notifications).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                type: 'Replied',
+                userId: userA.username,
+                tweetId: userAsTweet.id,
+                repliedBy: userB.username,
+                replyTweetId: userBsReply.id
+              })
+            ])
+          )
+        }, {
+          retries: 10,
+          maxTimeout: 1000
+        })
+      }, 15000)
+    })
+
+    describe("When user B mentions user A in a tweet", () => {
+      let userBsTweet
+      
+      beforeAll(async () => {
+        const text = `hey @${userAsProfile.screenName}`
+        userBsTweet = await when.a_user_calls_tweet(userB, text)
+      })
+
+      it('User A should receive a notification', async () => {
+        await retry(async () => {
+          expect(notifications).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                type: 'Mentioned',
+                userId: userA.username,
+                mentionedByTweetId: userBsTweet.id,
+                mentionedBy: userB.username
               })
             ])
           )
